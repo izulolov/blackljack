6b1163fd5dd97310ffe36e41f15701a62c9a99a1
izulolov <izulolov@mail.ru>
2023-08-15 17:41:03 +0300

    V odnom fayle. Initialize dlya Player, User, Dealer

---
+++ main.rb
@@ -0,0 +1,30 @@
+class Player
+  attr_reader :name, :balance
+  def initialize(name, balance)
+    @name = name
+    @balance = balance
+  end
+end
+
+class User < Player
+  def initialize(name, balance)
+    super(name, balance)
+  end
+end
+
+class Dealer < Player
+  def initialize
+    super('Dealer', 100)
+  end
+end
+
+
+
+=begin
+ur = User.new('Ikbol', 100)
+dr = Dealer.new
+puts ur.name
+puts dr.name
+puts ur.balance
+puts dr.balance
+=end
